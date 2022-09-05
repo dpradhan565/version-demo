c81abc717a8d19ca212eba66c13bfea1222cf574
Dhaneshwar Pradhan <dhaneshp@amdocs.com>
2022-09-05 13:06:05 +0530

    first commit

---
+++ src/test/java/com/dp/versiondemo/VersionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dp.versiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
